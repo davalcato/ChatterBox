b3416cba96f937bd0e00dbef4e0e0f5c11004523
davalcato <davalcato@yahoo.com>
2021-07-07 17:08:22 -0700

    Added the func setupUI to display the titleLabel.attributedText = attributedText on the viewController.

--- ChatterBox/ViewController.swift
+++ ChatterBox/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signinFacebookButton: UIButton!
+    @IBOutlet weak var signinGoogleButton: UIButton!
+    @IBOutlet weak var signinEmailButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    
+    @IBOutlet weak var orLabel: UILabel!
+    @IBOutlet weak var termsOfServiceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    // Declare the method
+    func setupUI() {
+        let title = "Create a new account"
+        let subtitle = "If using a storyboard, the `window` property will automatically be initia"
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font : UIFont.init(name: "Didot", size: 28)!,
+                                                                                   NSAttributedString.Key.foregroundColor : UIColor.brown
+                                                                                  ])
+        titleLabel.attributedText = attributedText
+        
     }
 
 
