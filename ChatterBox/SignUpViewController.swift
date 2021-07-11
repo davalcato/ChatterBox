27527571838c0943f661af9f3a45bf77c93f8a3c
davalcato <davalcato@yahoo.com>
2021-07-11 14:01:07 -0700

    Added the signUp screen and then added uiview, textfields, and the signIn field.

---
+++ ChatterBox/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  ChatterBox
+//
+//  Created by Daval Cato on 7/11/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
